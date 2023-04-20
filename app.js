8ab1910cdfca62d8175d009066824193d76dbb32
Joe-arg <joelreyes874@gmail.com>
2023-04-20 11:55:06 -0700

    04-20-2023

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const http = require('http');
 
 http.createServer((request, response) => {
-    console.log(request);
-    response.write('Hello World');
+    // console.log(request);
+    const person = {
+        id: 1,
+        name: 'Luis'
+    };
+    response.setHeader('Content-Disposition', 'attachment; filename=list.csv');
+    response.writeHead(200, {'Content-Type': 'application/csv'});
+    response.write('id, name\n');
+    response.write('1, Jose\n');
+    response.write('2, Luis\n');
+    response.write('3, Mary\n');
+    response.write('4, John\n');
+    response.write('5, Cris\n');
     response.end();
 }).listen(8080);
 
