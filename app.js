b320f278d0db7951edad5fd8fdc6b76d30514823
Joe-arg <joelreyes874@gmail.com>
2023-04-26 12:00:47 -0700

    04-26-2023

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    res.send('Home Page')
+    res.sendFile(__dirname + '/public/index.html')
 })
 
 app.get('/hi', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/test', (req, res) => {
+    const cdx = {
+        'name': 'Joel',
+        'last': 'Reyes'
+    }
+    res.render('test', cdx)
 })
 
 app.get('*', (req, res) => {
@@ -15,5 +26,5 @@ app.get('*', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`App listening on port ${port}`)
 })
